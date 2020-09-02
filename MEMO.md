b5e9e0b57644476b2ac0034cc89a5e9c86354821
KaitoTakase <48192611+kaitoooo@users.noreply.github.com>
2020-09-03 08:50:45 +0900

    first commit

---
+++ MEMO.md
@@ -0,0 +1,23 @@
+# package.json 作成
+
+npm init -y
+
+# webpack webpack-cli のインストール(package-lock.json 作成)
+
+npm install webpack webpack-cli --save
+
+# 生成コマンド
+
+npm run build
+
+# 監視コマンド
+
+npm run watch
+
+# swiper インストール
+
+npm -i swiper --save
+
+# vue インストール
+
+npm -i vue vue-loader vue-template-compiler --save
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+// output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出るproduction
+  mode: 'development',
+  // エントリーポイントの設定
+  entry: './src/js/main.js',
+  // 出力の設定
+  output: {
+    // 出力するファイル名
+    filename: 'js/[name].bundle.js',
+    // 出力先のパス（絶対パスを指定する必要がある）
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    // babel-loaderの設定
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+        // swiper使用時
+        // exclude: /node_modules\/(?!(dom7|ssr-window|swiper)\/).*/,
+      },
+      // SCSSをJSファイルと別々に生成する
+      {
+        test: /\.(sc|c|sa)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      // CSSでbackground-imageを使う
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/common/',
+              publicPath: function (path) {
+                return '../img/common/' + path;
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  //vue.jsの設定
+  // resolve: {
+  //   alias: {
+  //     vue$: 'vue/dist/vue.esm.js',
+  //   },
+  // },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '/css/[name].css',
+    }),
+  ],
+};
