f480b863ea0f4d8385509eea1fdda4a36edd30f8
KaitoTakase <48192611+kaitoooo@users.noreply.github.com>
2020-09-03 10:03:46 +0900

    update: entry

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ module.exports = {
   // 'development' | 'production'
   mode: process.env.NODE_ENV || 'development',
   // エントリーポイントの設定
-  entry: './src/js/main.js',
+  entry: {
+    main: [path.resolve(__dirname, 'src/js/main.js')],
+  },
   // 出力の設定
   output: {
     // 出力するファイル名
