618a2bb1a05de4e4fb1813bddd885ee4db684bd3
KaitoTakase <48192611+kaitoooo@users.noreply.github.com>
2020-09-03 09:44:31 +0900

    update: mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出るproduction
-  mode: 'development',
+  // 'development' | 'production'
+  mode: process.env.NODE_ENV || 'development',
   // エントリーポイントの設定
   entry: './src/js/main.js',
   // 出力の設定
