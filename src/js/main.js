29cac04db9e52eeec217c6e008371f67b4f81fb2
KaitoTakase <k-takase@vectorinc.co.jp>
2021-02-05 18:18:38 +0900

    update: js template

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 import '../styles/style.scss';
 import picturefill from 'picturefill';
 picturefill();
+
+export default class App {
+    constructor() {
+        window.addEventListener(
+            'DOMContentLoaded',
+            () => {
+                this.init();
+            },
+            false
+        );
+    }
+    init() {
+    }
+}
+new App();
